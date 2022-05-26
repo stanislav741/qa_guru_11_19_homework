0640ea0ec05b4560eb4e09eaa87599cb185e8a84
stanislav741 <stasdmitruk1@gmail.com>
2022-05-26 06:40:55 +0200

    Commented multiple rows for debugging purpose

--- src/test/java/tests/DemoShopTest.java
+++ src/test/java/tests/DemoShopTest.java
@@ -22,46 +22,46 @@ public class DemoShopTest extends TestBase {
     void loggedInWishlistCreateAndClear() {
 
         //Extracting an auth cookie for the test run
-        String authCookie =
-                given()
-                        .filter(withCustomTemplates())
-                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                        .formParam("Email", Credentials.email)
-                        .formParam("Password", Credentials.password)
-                .when()
-                        .post("http://demowebshop.tricentis.com/login")
-                .then()
-                        .statusCode(302)
-                        .extract().cookie("NOPCOMMERCE.AUTH");
+//        String authCookie =
+//                given()
+//                        .filter(withCustomTemplates())
+//                        .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+//                        .formParam("Email", Credentials.email)
+//                        .formParam("Password", Credentials.password)
+//                .when()
+//                        .post("http://demowebshop.tricentis.com/login")
+//                .then()
+//                        .statusCode(302)
+//                        .extract().cookie("NOPCOMMERCE.AUTH");
 
         //Applying the auth cookie to the WebDriver
         open("/Themes/DefaultClean/Content/images/logo.png");
-        getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", authCookie));
+//        getWebDriver().manage().addCookie(new Cookie("NOPCOMMERCE.AUTH", authCookie));
         open("");
 
         //Adding 5 products to the wishlist and verifying if the action is completed
-        for(int i=0; i<5; i++){
-            given()
-                    .filter(withCustomTemplates())
-                    .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                    .cookie("NOPCOMMERCE.AUTH", authCookie)
-            .when()
-                    .post("/addproducttocart/details/43/2")
-            .then()
-                    .statusCode(200)
-                    .body("success", is(true))
-                    .body("updatetopwishlistsectionhtml", not(is("()")));
-        }
-
-        //Opening the filled wishlist page and clearing all the items
-        $("#topcartlink").sibling(0).click();
-
-        $(byText("The wishlist is empty!")).shouldBe(hidden);
-
-        $(".remove-from-cart").click();
-        $(byName("updatecart")).click();
-
-        $(byText("The wishlist is empty!")).shouldBe(visible);
+//        for(int i=0; i<5; i++){
+//            given()
+//                    .filter(withCustomTemplates())
+//                    .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+//                    .cookie("NOPCOMMERCE.AUTH", authCookie)
+//            .when()
+//                    .post("/addproducttocart/details/43/2")
+//            .then()
+//                    .statusCode(200)
+//                    .body("success", is(true))
+//                    .body("updatetopwishlistsectionhtml", not(is("()")));
+//        }
+//
+//        //Opening the filled wishlist page and clearing all the items
+//        $("#topcartlink").sibling(0).click();
+//
+//        $(byText("The wishlist is empty!")).shouldBe(hidden);
+//
+//        $(".remove-from-cart").click();
+//        $(byName("updatecart")).click();
+//
+//        $(byText("The wishlist is empty!")).shouldBe(visible);
 
     }
 
